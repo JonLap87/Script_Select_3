8fa9dfadca0232c38fc371bccc1548103d85a8c2
Jon Lap <Lapchinevcer@mail.ru>
2023-03-19 11:15:20 +0300

    Corrected select

--- Script-select.sql
+++ Script-select.sql
@@ -1,17 +1,20 @@
 select NameAlbum, YearAlbum from Albums
-where YearAlbum = '2018-01-07';
+where YearAlbum = 2018;
 
 select NameTrack, Durarion from Tracks
-where Durarion = 5;
+ORDER by Durarion desc
+LIMIT 1;
+
 
 select NameTrack from Tracks
-where Durarion >  3.4;
+where Durarion >=  3.5;
 
-select NameCollect from Collections
-where YearCollect between '2018-04-04' and '2020-01-09';
+select NameCollect from collections
+where YearCollect between 2018 and 2020;
 
 select NameExecut from Executors
-where NameExecut = 'D';
+where not nameexecut like '%% %%';
+
 
 select NameTrack from Tracks
 where NameTrack like '%me%';
